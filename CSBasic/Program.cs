a6a1e5e0597846bec0ef9b624841b741ae1b579f
dahee90522 <s2018s40@e-mirim.hs.kr>
2020-04-28 14:22:03 +0900

    콘솔

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,15 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "윤" + "인" + "성";
+            Console.Write("Hello C#");
+            int add = 2;
+            int Alpha;
+            //주석
+            /*여러줄 주석*/
+            Console.WriteLine("Hello World");
+            Console.WriteLine("한빛 \t 아카데미");
         }
     }
 }
